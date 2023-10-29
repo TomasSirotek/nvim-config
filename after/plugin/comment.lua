13c8f5975a866b0bf78eb50bb641052bac38c123
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-29 20:30:37 +0100

    added commenting plugin confgi

---
+++ after/plugin/comment.lua
@@ -0,0 +1,24 @@
+require('Comment').setup(
+  {
+    ignore = '^$',
+    active = true,
+    on_config_done = nil,
+
+    padding = true,
+
+    toggler = {
+      line = 'gcc',
+      block = 'gbc',
+    },
+
+    opleader = {
+      line = 'gc',
+      block = 'gb',
+    },
+
+
+  }
+
+
+
+)
